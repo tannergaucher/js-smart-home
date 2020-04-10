437c432132a70f3a0be224eb19b1573d681a05d3
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-04-10 18:22:58 -0400

    finish oAuth with google

--- client/index.js
+++ client/index.js
@@ -1,21 +1,33 @@
-import "semantic-styles";
+import 'semantic-styles'
+import 'babel-polyfill'
 
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search)
+const authBtn = document.querySelector('#auth-btn')
+authBtn.addEventListener('click', authWithGoogle)
 
-if (urlParams.has("code")) {
-  const code = urlParams.get("code");
-
-  sendAccessToken(code);
+if (urlParams.has('code')) {
+  const code = urlParams.get('code')
+  sendAccessToken(code)
 }
 
-function sendAccessToken(code) {
-  const data = { code };
+async function authWithGoogle() {
+  const res = await fetch(`http://localhost:3000/login`)
+
+  if (res.ok) {
+    const { url } = await res.json()
+
+    window.location.href = url
+  }
+}
+
+async function sendAccessToken(code) {
+  const data = { code }
 
   fetch(`http://localhost:3000/code`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
-  });
+  })
 }
